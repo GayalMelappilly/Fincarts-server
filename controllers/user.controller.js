b4bd613f8292edb14959bda83699211993b9e244
Gayal Melappilly <gayalsunil@gmail.com>
2025-04-24 00:16:04 +0530

    feat: token based authenication setup

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 import client from '../config/db.js'
-import { addressInsertQuery, userInsertQuery } from '../query/user.query.js'
+import { addressInsertQuery, deleteRefreshToken, findRefreshTokenQuery, refreshTokenInsertQuery, userInsertQuery } from '../query/user.query.js'
+import { createAccessToken, createRefreshToken } from '../services/token.services.js'
 import { hashPassword } from '../utils/bcrypt.js'
 
 // Signup user
@@ -46,9 +47,29 @@ export const createProfile = async (req, res) => {
                 true
             ])
 
+            const accessToken = createAccessToken(userId)
+            const refreshToken = createRefreshToken(userId)
+
+            const refreshTokenResult = await client.query(refreshTokenInsertQuery, [
+                userId,
+                refreshToken,
+                new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+            ])
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'strict',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            })
+
             res.status(201).send({
                 success: true,
-                data: user
+                data: {
+                    name: user.FullName,
+                    email: user.email,
+                    phone: user.phone
+                }
             })
 
         } catch (err) {
@@ -68,3 +89,61 @@ export const createProfile = async (req, res) => {
         return;
     }
 }
+
+// Refresh token
+export const refresh = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (!refreshToken) {
+            return res.status(401).json({ message: 'Refresh token not found' });
+        }
+
+        const storedToken = await client.query(findRefreshTokenQuery, [
+            refreshToken
+        ])
+
+        if (!storedToken || storedToken.expiresAt < new Date()) {
+            return res.status(401).json({ message: 'Invalid or expired refresh token' });
+        }
+
+        const accessToken = createAccessToken(storedToken.userId);
+
+        res.status(201).json({
+            success: true,
+            accessToken
+        });
+
+    } catch (error) {
+        console.error('Refresh error:', error);
+        res.status(500).json({
+            success: false,
+            message: 'Server error'
+        });
+    }
+};
+
+// Logout user
+export const logout = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+
+        if (refreshToken) {
+            const result = await client.query(deleteRefreshToken, [
+                refreshToken
+            ])
+        }
+
+        res.clearCookie('refreshToken');
+        res.status(201).json({
+            success: true,
+            message: 'Logged out successfully'
+        });
+    } catch (error) {
+        console.error('Logout error:', error);
+        res.status(500).json({
+            sucess: false,
+            message: 'Server error'
+        });
+    }
+};
--- query/user.query.js
+++ query/user.query.js
@@ -1,3 +1,4 @@
+// Insert user data 
 export const userInsertQuery = `
     INSERT INTO users (
     email, 
@@ -12,7 +13,7 @@ export const userInsertQuery = `
   RETURNING *;
 `
 
-
+// Insert address details
 export const addressInsertQuery = `
     INSERT INTO user_addresses (
       user_id,
@@ -25,4 +26,26 @@ export const addressInsertQuery = `
       is_default
     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     RETURNING *;
+`
+
+// Insert refresh token
+export const refreshTokenInsertQuery = `
+    INSERT INTO refresh_tokens (
+      user_id,
+      token,
+      expires_at
+    ) VALUES ($1, $2, $3)
+     RETURNING *;
+`
+
+// Find refresh token
+export const findRefreshTokenQuery = `
+    SELECT * FROM refresh_tokens
+    WHERE token = $1;
+`
+
+// Delete refresh token
+export const deleteRefreshToken = `
+    DELETE FROM refresh_tokens
+    WHERE token = $1
 `
