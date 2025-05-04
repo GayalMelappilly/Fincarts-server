8aa33ff56198fc1bbe249e962915a0810df7f8c8
Gayal Melappilly <gayalsunil@gmail.com>
2025-05-05 02:37:42 +0530

    feat: change port to 5000

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ import http from 'http'
 import { app } from './app.js'
 import client from './config/db.js'
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 const server = http.createServer(app)
 
-server.listen(PORT, async () => {
+server.listen(5000, async () => {
     await client.connect().then(() => {
         console.log('Database connected successfully')
     }).catch((err) => {
