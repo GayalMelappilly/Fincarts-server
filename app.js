2cd9d2d916f6a44e295f0c7af3935bb7722de526
Gayal Melappilly <gayalsunil@gmail.com>
2025-06-26 22:48:38 +0530

    feat: add .com domain

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ export const app = express()
 app.use(express.json({ limit: '10mb' }))
 app.use(cookieParser())
 app.use(cors({
-    origin: ['https://fin-cart-web-client.vercel.app', 'http://localhost:3000'],
+    origin: ['https://fin-cart-web-client.vercel.app', 'http://localhost:3000', 'https://www.fincarts.com'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: [
